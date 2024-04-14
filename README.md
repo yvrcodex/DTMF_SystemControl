077b95738fc7258cdd81b02bccd30cfc17ff96aa
YASSER <yvrcodex@gmail.com>
2024-04-14 11:28:48 -0300

    initial strutcture

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# DTMF_SystemControl
-DTMF SYSTEM CONTROL is a project focused on utilizing DTMF modules for remote system control via audio. This MCU based system enables users to remotely control devices using DTMF tones generated through audio input.
+<h1 align="center">DTMF SYSTEM CONTROL</h1>
+
+---
+<p align="center">
+  <img src="https://img.shields.io/badge/Status-Active-4B0082?style=for-the-badge&logo=statuspage&logoColor=white" alt="Status Ativo">
+  <img src="https://img.shields.io/badge/License-MIT-blue.svg?style=for-the-badge&logo=mit&logoColor=white" alt="Licença MIT">
+  <img src="https://img.shields.io/badge/University-PUC%20MINAS-00599C?style=for-the-badge" alt="Universidade PUC MINAS">
+  <img src="https://img.shields.io/badge/Microcontroller-4B0082?style=for-the-badge&logo=microchip&logoColor=white" alt="Microcontroller">
+  <img src="https://img.shields.io/badge/Embedded%20System-4B0082?style=for-the-badge&logo=microchip&logoColor=white" alt="Embedded System">
+</p>
+
+# Table of Contents
+
+* PLACE HOLDER  INITIAL
+
+1. [Introduction](#introduction)
+2. [Workflow](#workflow)
+3. [Resources](#resources)
+4. [Features](#features)
+5. [Requirements](#requirements)
+6. [Installation](#installation)
+7. [Usage](#usage)
+8. [Contributors](#contributors)
+9. [License](#license)
+10. [Acknowledgements](#acknowledgements)
+11. [Support](#support)
+
+## Introduction
+
+The DTMF System Control project focuses on utilizing DTMF (Dual-Tone Multi-Frequency) modules for remote system control via audio. The project aims to enable users to remotely control devices, such as relays, using DTMF tones generated through audio input.
+
+## Workflow
+
+Here you can place the description, instructions, or any relevant information about the workflow of your project. This might include an explanation of how the DTMF modules are integrated with the Arduino, how to set up and configure the system, testing procedures, etc.
+
+## Resources
+
+- [Datasheets](docs/datasheets/README.md): Links to datasheets for the DTMF module, Arduino, and any other relevant components.
+- [Guides](docs/guides/README.md): Tutorials and guides for setting up and using the DTMF System Control.
+
+## Features
+
+- **Remote Control:** Enables remote control of devices via DTMF tones.
+- **Arduino Integration:** Utilizes Arduino microcontroller for processing DTMF signals.
+- **Customizable:** Easily adaptable for controlling various devices and applications.
+- **User-Friendly:** Simple interface for inputting DTMF commands via audio.
+
+## Requirements
+
+- **Hardware:** DTMF module, Arduino (e.g., ATmega328P), audio input/output device.
+- **Software:** Arduino IDE, libraries for DTMF decoding.
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/yvrcodex/DTMF_SystemControl
---
+++ src/main.cpp
@@ -0,0 +1,130 @@
+// include PlatformIO extension VSCODE
+#include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define BAUD 9600
+#define MYUBRR F_CPU / 16 / BAUD - 1
+
+// INICIAR COMUNICAÇÃO SERIAL VIA REGISTER UBBR
+int x = 1;
+
+void USART_Init(unsigned int ubrr)
+{
+    UBRR0H = (unsigned char)(ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void USART_Transmit(unsigned char data)
+{
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = data;
+}
+
+void USART_Println(const char *s)
+{
+    while (*s)
+    {
+        USART_Transmit(*s++);
+    }
+    USART_Transmit('\r');
+    USART_Transmit('\n');
+}
+
+// FUNÇÃO PRINT USANDO FUNCTION
+
+void printDTMF(char key, const char *tone)
+{
+    char buffer[50];
+    sprintf(buffer, "Tecla: %c, Tons DTMF: %s", key, tone);
+    USART_Println(buffer);
+}
+
+int main(void)
+{
+    DDRD &= ~((1 << DDD3) | (1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 << DDD7)); // Configura os pinos D3 a D7 como entrada
+    USART_Init(MYUBRR);
+
+    while (1)
+    {
+        uint8_t number;
+
+        bool signal = PIND & (1 << PIND7); // leitura pino D7
+
+        if (signal)
+        {
+            _delay_ms(200);              // Delay para debouncing
+            number = (PIND & 0x78) >> 3; // leitura dos  pinos D3 a D6 e ajusta a posição dos bits
+            switch (number)
+            {
+
+            case 0x01: // b0001
+
+                printDTMF('1', "697 Hz e 1209 Hz");
+
+                break;
+
+            case 0x02: // b0010
+
+                printDTMF('2', "697 Hz e 1336 Hz");
+                break;
+
+            case 0x03: // b0011
+                printDTMF('3', "697 Hz e 1477 Hz");
+                break;
+
+            case 0x04: // b0100
+
+                printDTMF('4', "770 Hz e 1209 Hz");
+                break;
+
+            case 0x05: // b0101
+
+                printDTMF('5', "770 Hz e 1336 Hz");
+                break;
+
+            case 0x06: // b0110
+
+                printDTMF('6', "770 Hz e 1477 Hz");
+                break;
+
+            case 0x07: // b0111
+
+                printDTMF('7', "852 Hz e 1209 Hz");
+                break;
+
+            case 0x08: // b1000
+                printDTMF('8', "852 Hz e 1336 Hz");
+                break;
+
+            case 0x09: // b1001
+
+                printDTMF('9', "852 Hz e 1477 Hz");
+                break;
+
+            case 0x0A: // b1010
+
+                printDTMF('0', "941 Hz e 1336 Hz");
+                break;
+
+            case 0x0B: // b1011
+            
+                printDTMF('*', "941 Hz e 1209 Hz");
+                break;
+
+            case 0x0C: // b1100
+                printDTMF('#', "941 Hz e 1477 Hz");
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
+
+/* IMplementação bateria de teste. Comparação de saidas */
