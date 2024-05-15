3dad7f724987370bfff17228aad6f45a735dceaf
YASSER <yvrcodex@gmail.com>
2024-05-15 18:15:24 -0300

    v.1.0/ finish

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,28 @@
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>             // Arduino SPI librar
 
+
+
 // Definições dos pinos do display ST7789
-#define TFT_DC    9            // DC (data/command)
-#define TFT_RST   8            // RST (reset)
-#define TFT_MOSI  11           // MOSI (SPI data pin)
-#define TFT_SCLK  13           // SCLK (SPI sclk pin)
-#define TFT_CS    10           // CHIP SELECT
+#define TFT_DC 9    // DC (data/command)  DC
+#define TFT_RST 8   // RST (reset) RES
+#define TFT_MOSI 11 // MOSI (SPI data pin) SDA
+#define TFT_SCLK 13 // SCLK (SPI sclk pin) SCK
+#define TFT_CS 10   // CHIP SELECT  NC NULL
+
+// DUAL TONE CHARACTERE DEFINES
+#define DTMF_1          0x01        //  1 - (frequências: 697 Hz e 1336 Hz)
+#define DTMF_2          0x02        //  2 - (frequências: 697 Hz e 1477 Hz)
+#define DTMF_3          0x03        //  3 - (frequências: 770 Hz e 1209 Hz)
+#define DTMF_4          0x04        //  4 - (frequências: 770 Hz e 1336 Hz)
+#define DTMF_5          0x05        //  5 - (frequências: 770 Hz e 1477 Hz)
+#define DTMF_6          0x06        //  6 - (frequências: 852 Hz e 1209 Hz)
+#define DTMF_7          0x07        //  7 - (frequências: 852 Hz e 1336 Hz)
+#define DTMF_8          0x08        //  8 - (frequências: 852 Hz e 1477 Hz)
+#define DTMF_9          0x09        //  9 - (frequências: 941 Hz e 1209 Hz)
+#define DTMF_0          0x0A        //  0 - (frequências: 697 Hz e 1209 Hz)
+#define DTMF_HASH       0x0B        //  # - (frequências: 941 Hz e 1477 Hz)
+#define DTMF_ASTERISK   0x0C        // .* - (frequências: 941 Hz e 1209 Hz)
 
 
 // MODULO DTMF MT8870 PINOUT
@@ -19,57 +35,38 @@
 #define STQ     PC4                 //  OUTPUT STATE      - Pino A4 do Arduino Uno (PORTC, Bit 4)
 
 // PINO PARA INTERRUPÇÃO
-#define PIN_INTERRUPT PCINT21       // PD5 [ PINO 5 ARDUINO UNO]
+#define PIN_INTERRUPT PCINT21  // PD5 [ PINO 5 ARDUINO UNO]
 
-// DUAL TONE CHARACTERE DEFINES
-#define DTMF_1          0x11        //  1 - (frequências: 697 Hz e 1336 Hz)
-#define DTMF_2          0x12        //  2 - (frequências: 697 Hz e 1477 Hz)
-#define DTMF_3          0x13        //  3 - (frequências: 770 Hz e 1209 Hz)
-#define DTMF_4          0x14        //  4 - (frequências: 770 Hz e 1336 Hz)
-#define DTMF_5          0x15        //  5 - (frequências: 770 Hz e 1477 Hz)
-#define DTMF_6          0x16        //  6 - (frequências: 852 Hz e 1209 Hz)
-#define DTMF_7          0x17        //  7 - (frequências: 852 Hz e 1336 Hz)
-#define DTMF_8          0x18        //  8 - (frequências: 852 Hz e 1477 Hz)
-#define DTMF_9          0x19        //  9 - (frequências: 941 Hz e 1209 Hz)
-#define DTMF_0          0x1A        //  0 - (frequências: 697 Hz e 1209 Hz)
-#define DTMF_HASH       0x1B        //  # - (frequências: 941 Hz e 1477 Hz)
-#define DTMF_ASTERISK   0x1C        // .* - (frequências: 941 Hz e 1209 Hz)
-
-// CONSTANT DEFINE
 #define KEYWORD_MAX_LENGTH  4       // SEQUENCIA MAXIMA de tons DTMF
 #define CURSOR_POS0_X      20       // POSIÇÃO INICIAL CURSOR DISPLAY COLUNA
 #define CURSOR_POS0_Y      30       // POSIÇÃO INICIAL CURSOR DISPLAY LINHA
 #define CURSOR_INCREMENT_X 50       // INCREMENTO INICIAL PARA  CURSOR  X LINHA
 
-// Instância Adafruit ST7789 TFT library
+// Instância da Adafruit ST7789 TFT library
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 /* DECLARAÇÃO DE PROTOTIPOS */
-bool    check_password(uint8_t *password);
-uint8_t disp_asterisk(bool x);
+uint8_t check_password(uint8_t *password, uint8_t keyword_read[]);
 uint8_t dtmf_dispChar (uint8_t keyword_value);
 uint8_t disp_asterisk(bool x);
 uint8_t relay_enable(uint8_t active);
 uint8_t relay_disable(uint8_t desactive);
+void    invalid_keyword(void);
 void    loading(void);
 void    reading(void);
-void    invalid(void);
 
-/* // PASSWORD TEST
-uint8_t TEST_password_A1[] = {DTMF_1, DTMF_8, DTMF_2, DTMF_6};        
-uint8_t TEST_password_A2[] = {DTMF_5, DTMF_HASH, DTMF_3}; 
-*/
+
 
 // KEYWORD: DECLARAÇÃO DOS PASSWORDS
-uint8_t password_A1E[] = {DTMF_1, DTMF_8, DTMF_2, DTMF_6};   // PASSWORD: ATIVA RELÉ A1
-uint8_t password_A2E[] = {DTMF_5, DTMF_0, DTMF_3, DTMF_5};   // PASSWORD: ATIVA RELÉ A2
-uint8_t password_A3E[] = {DTMF_5, DTMF_3, DTMF_3, DTMF_5};   // PASSWORD: ATIVA RELÉ A3
-uint8_t password_A4E[] = {DTMF_5, DTMF_7, DTMF_3, DTMF_5};   // PASSWORD: ATIVA RELÉ A4
+uint8_t password_A1E[] = {DTMF_1, DTMF_2, DTMF_3, DTMF_4};   // PASSWORD: ATIVA RELÉ A1
+uint8_t password_A2E[] = {DTMF_3, DTMF_4, DTMF_5, DTMF_6};   // PASSWORD: ATIVA RELÉ A2
+uint8_t password_A3E[] = {DTMF_5, DTMF_6, DTMF_7, DTMF_8};   // PASSWORD: ATIVA RELÉ A3
+uint8_t password_A4E[] = {DTMF_7, DTMF_8, DTMF_9, DTMF_0};   // PASSWORD: ATIVA RELÉ A4
 
-uint8_t password_A1D[] = {DTMF_1, DTMF_8, DTMF_2, DTMF_6};   // PASSWORD: DESATIVA RELÉ A1
-uint8_t password_A2D[] = {DTMF_5, DTMF_0, DTMF_3, DTMF_5};   // PASSWORD: DESATIVA RELÉ A2
-uint8_t password_A3D[] = {DTMF_5, DTMF_3, DTMF_3, DTMF_5};   // PASSWORD: DESATIVA RELÉ A3
-uint8_t password_A4D[] = {DTMF_5, DTMF_7, DTMF_3, DTMF_5};   // PASSWORD: DESATIVA RELÉ A4
+uint8_t password_A1D[] = {DTMF_1, DTMF_2, DTMF_3, DTMF_3};   // PASSWORD: DESATIVA RELÉ A1
+uint8_t password_A2D[] = {DTMF_3, DTMF_4, DTMF_5, DTMF_HASH};   // PASSWORD: DESATIVA RELÉ A2
+uint8_t password_A3D[] = {DTMF_5, DTMF_6, DTMF_7, DTMF_HASH};   // PASSWORD: DESATIVA RELÉ A3
+uint8_t password_A4D[] = {DTMF_7, DTMF_8, DTMF_9, DTMF_HASH};   // PASSWORD: DESATIVA RELÉ A4
 
 // DECLARAÇÃO VARIAVEIS GLOBAL;
 uint8_t keyword_read[KEYWORD_MAX_LENGTH];
@@ -77,105 +74,111 @@ uint8_t keyword_sequence = 0;
 uint8_t keyword;
 uint8_t cursor_posX = CURSOR_POS0_X;
 uint8_t cursor_posY = CURSOR_POS0_Y;
+volatile bool flag = 0;
 
 
-//================ MAIN FUNCTION ==================================
+
+
 void setup(void)
 {
 
-  UCSR0B  &= ~0x18;            // desabilita RX (bit4) e TX(bit3) para trabalho com os pinos do PORTD no Arduino
-  DDRD    |= 0xC0;             // Define os bits 6 e 7 como saída (11000000 em binário)
-  PORTD    = 0x00;             // garante os pinos PD0 à PD7 iniciem em LOW
-  DDRC    &= ~0x7F;            // DEFINE PORTC COMO ENTRADA
-  PORTC   &= ~0x7F;            // Garante que os pinos A0 à A6 iniciem em zero;
-  PCICR   |= (1 << PCIE2);     // Habilita a interrupção via PORTD (PCINT16-23)
-  PCMSK2  |= (1 << PCINT21);   // habilita interrupção para PCINT21 (PD5) (pino 5 arduino)
-  SREG    |= 0x80;             // Habilitar interrupções globais bit 7 (I = 1)
 
-  //INITIAL SETUP
+  UCSR0B   =   0x00; /*necessário desabilitar RX e TX para trabalho com os pinos do PORTD no Arduino*/
+  PORTC    =   0xFF; // inicia pinos analogicos em zero;
+  DDRC    &=   0X3F; // DEFINE OS PINOS A0 À A4 COMO ENTRADA;
+  DDRD    |=   0xC0; // DEFINE PINOS 6 E 7 COMO SAIDAS;
+
+
+    
+  PCICR |= (1 << PCIE2);          // Habilitar a interrupção PCINT
+  PCMSK2 |= (1 << PIN_INTERRUPT); // Habilitar a interrupção PINO 5 ARDUINO
+  //sei();                          // Habilitar interrupções globais
+
+
   Serial.begin(9600);
   tft.init(240, 240, SPI_MODE2);
   tft.setRotation(2);
   tft.fillScreen(ST77XX_BLACK);
   loading();
   reading();
-  disp_asterisk(true);
-  _delay_ms(3000);
+  tft.fillScreen(ST77XX_BLACK);
+  disp_asterisk(1);
+  _delay_ms(1000);
 
-
-  while (true) // LOOP LEITURA DTMF KEYS
+while (true) // LOOP LEITURA DTMF KEYS
   {
 
-    bool signal = (PINC |= (1 << STQ));              // Lê o estado do STQ
+    bool signal = (PINC &(1 << STQ));              // Lê o estado do STQ
 
     if (signal) 
     {
-     
-      keyword = (PINC);                             // ADD AO KYWORD O VALOR NO PINC
 
-      if (keyword_sequence < KEYWORD_MAX_LENGTH)
-      {
+        keyword = (PINC & 0x0F);   
+        _delay_ms(100);                          // ADD AO KYWORD O VALOR NO PINC
+        if (keyword_sequence < KEYWORD_MAX_LENGTH)
+        {
 
-        keyword_read[keyword_sequence++] = keyword; // ADD AO ARRAY O VALOR KEYWORD E INCREMENTA POSIÇÃO
-        dtmf_dispChar(keyword);                     // MOSTRA NO DISPLAY O TON RECEBEIDO
-        cursor_posX+= CURSOR_INCREMENT_X;           // MUDA A POSIÇÃO DO CURSOR
-
-      }//fim keyword_read
-
-      else
-      {
-
-      PORTD |= (1 << PIN_INTERRUPT); // ATIVA INTERRUPÇÃO  
-
-      tft.fillScreen(ST77XX_BLACK);  // LIMPA A TELA;
-      
-      }// fim else
+          keyword_read[keyword_sequence++] = keyword; // ADD AO ARRAY O VALOR KEYWORD E INCREMENTA POSIÇÃO
+          disp_asterisk(0);
+          dtmf_dispChar(keyword);                     // MOSTRA NO DISPLAY O TON RECEBEIDO
+          cursor_posX+= CURSOR_INCREMENT_X;           // MUDA A POSIÇÃO DO CURSOR
+        _delay_ms(100);
+        }//fim keyword_read
     }
-    _delay_ms(300);
+    else if (!signal && !(keyword_sequence < KEYWORD_MAX_LENGTH))
+    {
+
+    sei(); 
+    flag = 1;
+    PORTD |= (1 << PIN_INTERRUPT); // ATIVA INTERRUPÇÃO
+    _delay_ms(2000);
+    keyword_sequence = 0;
+    cursor_posX = CURSOR_POS0_X;
+    cursor_posY = CURSOR_POS0_Y;
+    tft.fillScreen(ST77XX_BLACK);
+    disp_asterisk(1);
+    
+    }// fim else
   } // fim leitura signal
+
 } // fim main
 
 
 
-// ======================= INTERUPTION FUNCTION ======================
+ISR(PCINT2_vect)
+{
 
-ISR(PCINT2_vect){
+  if(flag){
 
-    if(PIND & (1 << PIN_INTERRUPT)){ // VERIFICA SE A INTERRUPÇÃ OCORREU NO PINO DEFINIDO
-
-    // VERIFICA SE A SEQUENCIA LIDA EQUIVALE À ALGUM PASSWORD
-
-    if      (check_password(password_A1E))      relay_enable(1); 
-
-    else if (check_password(password_A2E))      relay_enable(2);
-
-    else if (check_password(password_A3E))      relay_enable(3);
-
-    else if (check_password(password_A4E))      relay_enable(4);
-
-    else if (check_password(password_A1D))      relay_disable(1);
-
-    else if (check_password(password_A2D))      relay_disable(2);
+    if      (check_password(password_A1E,keyword_read))   relay_enable(1);
     
-    else if (check_password(password_A3D))      relay_disable(3);
-    
-    else if (check_password(password_A4D))      relay_disable(4);
-    
-    else    invalid();
+    else if (check_password(password_A2E,keyword_read))   relay_enable(2);
 
-  }
+    else if (check_password(password_A3E,keyword_read))   relay_enable(3);
 
-  PCIFR &= ~(1 << PCIF2);  // Limpa o bit de flag de interrupção do Port C (PCINT16-23)
+    else if (check_password(password_A4E,keyword_read))   relay_enable(4);
 
-  loading();
-  
+    else if (check_password(password_A1D,keyword_read))   relay_disable(1);
+
+    else if (check_password(password_A2D,keyword_read))   relay_disable(2);
+
+    else if (check_password(password_A3D,keyword_read))   relay_disable(3);
+
+    else if (check_password(password_A4D,keyword_read))   relay_disable(4);
+
+  else invalid_keyword();
+
+    }
+
+  flag = 0;
+  PCIFR |= (1 << PCIF2);
+  cli();
+  PORTD &= ~(1 << PIN_INTERRUPT); 
+
 }
 
-
-//===================== PROTOTIPE FUNCTIONs =========================================
-
 /* PASSWORD CHECK */
-bool check_password(uint8_t *password) // Função para verificar se a sequência de tons corresponde a uma senha
+uint8_t check_password(uint8_t *password, uint8_t keyword_read[]) // Função para verificar se a sequência de tons corresponde a uma senha
 {
   for (uint8_t i = 0; i < KEYWORD_MAX_LENGTH; i++)
   {
@@ -186,16 +189,11 @@ bool check_password(uint8_t *password) // Função para verificar se a sequênci
 } //fim check_password
 
 
-
-/* TFT DISPLAY CHARACTER */
 uint8_t dtmf_dispChar (uint8_t keyword_value){
 
 
-  extern uint8_t cursor_posX;
-  extern uint8_t cursor_posY;
-
   tft.setTextSize(3);
-  tft.setCursor(cursor_posX, cursor_posY);
+  tft.setCursor(cursor_posX, CURSOR_POS0_Y);
   tft.setTextColor(ST77XX_BLACK);
   tft.println(" * ");
   tft.setTextColor(ST77XX_YELLOW);
@@ -232,174 +230,9 @@ uint8_t dtmf_dispChar (uint8_t keyword_value){
   break;
   }
 
-  tft.setTextColor(ST77XX_YELLOW);
-  tft.setCursor(cursor_posX, cursor_posY);
-
-  return keyword_value;
-}
-
-/* PASSWORD ASTERISK */
-uint8_t disp_asterisk(bool x){
-
-
-  extern uint8_t cursor_posX;
-  extern uint8_t cursor_posY;
-
-  uint8_t set_cursor = cursor_posX;
-  
-  tft.setCursor(set_cursor, cursor_posY);
-
-  tft.setTextSize(3);
-  tft.setTextColor(ST77XX_RED);
-  
-
-  if(x){
-
-  for (int i = 0; i < KEYWORD_MAX_LENGTH; i++)
-  {
-
-  tft.setCursor(set_cursor, cursor_posY);
-  tft.println(" * ");
-  set_cursor += CURSOR_INCREMENT_X;
-  }
-
-
-  }
-  else{
-  
-  tft.setTextSize(3);
-  tft.setTextColor(ST77XX_BLACK);
-  tft.setCursor(cursor_posX, cursor_posY);
-  tft.println(" * ");
-  }
-  return x;
 }
 
 
-uint8_t relay_enable(uint8_t enable){
-
-  tft.setTextColor(ST77XX_GREEN); 
-
-  if  (enable == 1)
-  {
-
-  tft.setTextSize(6); 
-  tft.setCursor(83, 100);
-  tft.println("A1");
-
-  tft.setTextSize(3); 
-  tft.setCursor(35, 150);
-  tft.println("ENABLED");
-  }
-
-  else if (enable ==2)
-  {
-
-  tft.setTextSize(6); 
-  tft.setCursor(83, 100);
-  tft.println("A2 ");
-
-  tft.setTextSize(3); 
-  tft.setCursor(35, 150);
-  tft.println("ENABLED");
-
-  }
-  else if (enable == 3)
-  {
-
-  tft.setTextSize(6); 
-  tft.setCursor(83, 100);
-  tft.println("A3");
-
-  tft.setTextSize(3); 
-  tft.setCursor(35, 150);
-  tft.println("ENABLED");
-
-  }else if(enable == 4) {
-
-    
-  tft.setTextSize(6); 
-  tft.setCursor(83, 100);
-  tft.println("A4");
-
-  tft.setTextSize(3); 
-  tft.setCursor(35, 150);
-  tft.println("ENABLED");
-
-  }
-  return enable;
-}
-
-uint8_t relay_disable(uint8_t disable){
-
-  tft.setTextColor(ST77XX_GREEN); 
-
-  if(disable == 1)
-  {
-
-  tft.setTextSize(6); 
-  tft.setCursor(83, 100);
-  tft.println("A1");
-
-  tft.setTextSize(3); 
-  tft.setCursor(35, 150);
-  tft.println("DISABLE");
-  }
-
-  else if (disable ==2)
-  {
-
-  tft.setTextSize(6); 
-  tft.setCursor(83, 100);
-  tft.println("A2 ");
-
-  tft.setTextSize(3); 
-  tft.setCursor(35, 150);
-  tft.println("DISABLE");
-
-  }
-  else if (disable == 3)
-  {
-
-  tft.setTextSize(6); 
-  tft.setCursor(83, 100);
-  tft.println("A3");
-
-  tft.setTextSize(3); 
-  tft.setCursor(35, 150);
-  tft.println("DISABLE");
-
-  }else if (disable == 4) {
-
-    
-  tft.setTextSize(6); 
-  tft.setCursor(83, 100);
-  tft.println("A4");
-
-  tft.setTextSize(3); 
-  tft.setCursor(35, 150);
-  tft.println("DISABLE");
-
-  }
-  return disable;
-}
-
-void invalid(void){
-
-
-  tft.fillScreen(ST77XX_RED);
-  tft.setTextColor(ST77XX_WHITE);
-
-  tft.setTextSize(3); 
-  tft.setCursor(35, 130);
-  tft.println("INVALID");
-  tft.setCursor(35, 150);
-  tft.println("PASSWORD");
-
-  _delay_ms(1000);
-  disp_asterisk(true);
-
-}
 
 /* ANIMATION LOADING */
 void loading(void)
@@ -434,7 +267,7 @@ void loading(void)
   tft.fillScreen(ST77XX_BLACK);
 }
 
-/* ANIMATION READING */
+//======================= TELA DE LEITURA ==========================
 void reading(void)
 {
 
@@ -460,7 +293,7 @@ void reading(void)
   tft.setCursor(35, 45);
   tft.println("DUAL-TONE");
 
-  // Loop para desenhar os pontos em um círculo
+
 
     int i = 0;
     int j = 1;
@@ -492,11 +325,201 @@ void reading(void)
       tft.fillCircle(x1, y1, dotSize / 2, dotColor);
       j++;
 
-    }
+  } // fim while
+}
+/* PASSWORD ASTERISK */
+uint8_t disp_asterisk(bool x){
+
+
+  uint8_t set_cursor = cursor_posX;
+  
+  tft.setCursor(set_cursor, cursor_posY);
+
+  tft.setTextSize(3);
+  tft.setTextColor(ST77XX_RED);
+  
+
+  if(x){
+
+  for (int i = 0; i < KEYWORD_MAX_LENGTH; i++)
+  {
+
+  tft.setCursor(set_cursor, cursor_posY);
+  tft.println(" * ");
+  set_cursor += CURSOR_INCREMENT_X;
+  }
+
+
+  }
+  else{
+  
+  tft.setTextSize(3);
+  tft.setTextColor(ST77XX_BLACK);
+  tft.setCursor(cursor_posX, cursor_posY);
+  tft.println(" * ");
+  }
+  return x;
 }
 
 
-/* SCRIPT TEST DISPLAY 
+uint8_t relay_enable(uint8_t enable){
+
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_GREEN);
+
+
+  uint8_t set_textsizeA = 9;
+  uint8_t set_cursorAX = 70;
+  uint8_t set_cursorAY = 40;
+
+  uint8_t set_textE = 4;
+  uint8_t set_cursorEX = 35;
+  uint8_t set_cursorEY = 150;
+
+
+
+  if  (enable == 1)
+  {
+
+  tft.setTextSize(set_textsizeA); 
+  tft.setCursor(set_cursorAX, set_cursorAY);
+  tft.println("A1");
+
+  tft.setTextSize(set_textE); 
+  tft.setCursor(set_cursorEX, set_cursorEY);
+  tft.println("ENABLED");
+  }
+
+  else if (enable ==2)
+  {
+
+  tft.setTextSize(set_textsizeA); 
+  tft.setCursor(set_cursorAX, set_cursorAY);
+  tft.println("A2 ");
+
+  tft.setTextSize(set_textE); 
+  tft.setCursor(set_cursorEX, set_cursorEY);
+  tft.println("ENABLED");
+
+  }
+  else if (enable == 3)
+  {
+
+  tft.setTextSize(set_textsizeA); 
+  tft.setCursor(set_cursorAX, set_cursorAY);
+  tft.println("A3");
+
+  tft.setTextSize(set_textE); 
+  tft.setCursor(set_cursorEX, set_cursorEY);
+  tft.println("ENABLED");
+
+  }else if(enable == 4) {
+
+    
+  tft.setTextSize(set_textsizeA); 
+  tft.setCursor(set_cursorAX, set_cursorAY);
+  tft.println("A4");
+
+  tft.setTextSize(set_textE); 
+  tft.setCursor(set_cursorEX, set_cursorEY);
+  tft.println("ENABLED");
+
+  }
+  return enable;
+}
+
+uint8_t relay_disable(uint8_t disable){
+
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_RED);
+
+  uint8_t set_textsizeA = 9;
+  uint8_t set_cursorAX = 70;
+  uint8_t set_cursorAY = 40;
+
+  uint8_t set_textE = 4;
+  uint8_t set_cursorEX = 35;
+  uint8_t set_cursorEY = 150;
+
+
+
+  if  (disable == 1)
+  {
+
+  tft.setTextSize(set_textsizeA); 
+  tft.setCursor(set_cursorAX, set_cursorAY);
+  tft.println("A1");
+
+  tft.setTextSize(set_textE); 
+  tft.setCursor(set_cursorEX, set_cursorEY);
+  tft.println("DISABLE");
+  }
+
+  else if (disable == 2)
+  {
+
+  tft.setTextSize(set_textsizeA); 
+  tft.setCursor(set_cursorAX, set_cursorAY);
+  tft.println("A2 ");
+
+  tft.setTextSize(set_textE); 
+  tft.setCursor(set_cursorEX, set_cursorEY);
+  tft.println("DISABLE");
+
+  }
+  else if (disable == 3)
+  {
+
+  tft.setTextSize(set_textsizeA); 
+  tft.setCursor(set_cursorAX, set_cursorAY);
+  tft.println("A3");
+
+  tft.setTextSize(set_textE); 
+  tft.setCursor(set_cursorEX, set_cursorEY);
+  tft.println("DISABLE");
+
+  }else if(disable == 4) {
+
+    
+  tft.setTextSize(set_textsizeA); 
+  tft.setCursor(set_cursorAX, set_cursorAY);
+  tft.println("A4");
+
+  tft.setTextSize(set_textE); 
+  tft.setCursor(set_cursorEX, set_cursorEY);
+  tft.println("DISABLE");
+  }
+  return disable;
+}
+
+void invalid_keyword(void){
+
+  uint8_t set_textsizeA = 4;
+  uint8_t set_cursorAX = 35;
+  uint8_t set_cursorAY = 80;
+
+  uint8_t set_textE = 4;
+  uint8_t set_cursorEX = 30;
+  uint8_t set_cursorEY = 130; 
+
+  tft.fillScreen(ST77XX_RED);
+  tft.setTextColor(ST77XX_WHITE);
+
+  tft.setTextSize(set_textsizeA); 
+  tft.setCursor(set_cursorAX, set_cursorAY);
+  tft.println("INVALID");
+  tft.setCursor(set_cursorEX, set_cursorEY);
+  tft.println("PASSWORD");
+
+  _delay_ms(1000);
+  disp_asterisk(true);
+
+}
+
+
+
+
+/*
 void reading(void){
 
 
@@ -523,6 +546,7 @@ void reading(void){
   tft.println(".");
   _delay_ms(500);
 }
+/*
 bool password_display(bool test){
 
 
@@ -570,7 +594,7 @@ bool password_display(bool test){
 
 }
 
-
+/*
 void testlines(uint16_t color)
 {
   tft.fillScreen(ST77XX_BLACK);
@@ -789,4 +813,3 @@ void mediabuttons()
   // play color
   tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_GREEN);
 } */
-
